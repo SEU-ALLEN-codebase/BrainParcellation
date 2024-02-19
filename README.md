8c83c747331652c4df388fdca9bb95ded37204a7
crazylyf <crazylyf@163.com>
2024-02-19 11:20:47 +0800

    tuned and updated the Leiden-based partition

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BrainParcellation
 Tools for generating whole brain parcellation through ensembled morphological features
+
+### TODO
+- [ ] L-R mirroring of coordinates while calculating microenvironment features
--- microenviron/parcellation.py
+++ microenviron/parcellation.py
@@ -13,18 +13,27 @@ import pickle
 from collections import defaultdict
 from sklearn.neighbors import kneighbors_graph, radius_neighbors_graph
 from scipy.sparse import csr_matrix
+import matplotlib as mpl
+import matplotlib.cm as cm
+import cv2
 
 import leidenalg as lg
 import igraph as ig
 
+from image_utils import get_mip_image, image_histeq
+from file_io import load_image, save_image
+from anatomy.anatomy_config import MASK_CCF25_FILE
+
+from generate_me_map import process_mip
 
 
 class BrainParcellation:
-    def __init__(self, mefile, scale=25., full_features=True):
-        self.df = self.load_features(mefile, scale=scale, full_features=full_features)
+    def __init__(self, mefile, scale=25., full_features=True, flipLR=True, seed=1024):
+        self.df = self.load_features(mefile, scale=scale, full_features=full_features, flipLR=flipLR)
+        np.random.seed(seed)
 
 
-    def load_features(self, mefile, scale=25., full_features=True):
+    def load_features(self, mefile, scale=25., full_features=True, flipLR=True):
         df = pd.read_csv(mefile, index_col=0)
         
         if full_features:
@@ -51,7 +60,68 @@ class BrainParcellation:
         df = df[in_region]
         print(f'Filtered out {in_region.shape[0] - df.shape[0]}')
 
+        if flipLR:
+            # mirror right hemispheric points to left hemisphere
+            zdim2 = zdim // 2
+            nzi = np.nonzero(df['soma_z'] < zdim2)
+            loci = df.index[nzi]
+            df.loc[loci, 'soma_z'] = zdim - df.loc[loci, 'soma_z']
+
         return df
+
+    @staticmethod
+    def visualize_on_ccf(dfp):
+        mask = load_image(MASK_CCF25_FILE)  # z,y,x order!
+        shape3d = mask.shape
+        zdim2, ydim2, xdim2 = shape3d[0]//2, shape3d[1]//2, shape3d[2]//2
+
+        # map the communities to different colors using randomized color map
+        norm = mpl.colors.Normalize(vmin=dfp['parc'].min(), vmax=dfp['parc'].max())
+        vals = np.linspace(0,1,5120)
+        np.random.shuffle(vals)
+        cmap = cm.colors.ListedColormap(cm.jet(vals))
+        #cmap = cm.bwr
+        smapper = cm.ScalarMappable(norm=norm, cmap=cmap)
+        colors = np.floor(smapper.to_rgba(dfp['parc']) * 255).astype(np.uint8)
+        xyz = np.floor(dfp[['soma_x', 'soma_y', 'soma_z']].to_numpy()).astype(np.int32)
+
+        # intialize map
+        pmap = np.zeros((*shape3d, colors.shape[-1]), dtype=np.uint8)
+        pmap[xyz[:,2], xyz[:,1], xyz[:,0]] = colors
+
+        
+        thickX2 = 20
+        for axid in range(3):
+            print(f'--> Processing axis: {axid}')
+            cur_map = pmap.copy()
+            if thickX2 != -1:
+                if axid == 0:
+                    cur_map[:zdim2-thickX2] = 0
+                    cur_map[zdim2+thickX2:] = 0
+                elif axid == 1:
+                    cur_map[:,:ydim2-thickX2] = 0
+                    cur_map[:,ydim2+thickX2:] = 0
+                else:
+                    cur_map[:,:,:xdim2-thickX2] = 0
+                    cur_map[:,:,xdim2+thickX2:] = 0
+            print(cur_map.mean(), cur_map.std())
+
+            mip = get_mip_image(cur_map, axid)
+            figname = f'temp_mip{axid}.png'
+            process_mip(mip, mask, axis=axid, figname=figname, mode='composite')
+            # load and remove the zero-alpha block
+            img = cv2.imread(figname, cv2.IMREAD_UNCHANGED)
+            wnz = np.nonzero(img[img.shape[0]//2,:,-1])[0]
+            ws, we = wnz[0], wnz[-1]
+            hnz = np.nonzero(img[:,img.shape[1]//2,-1])[0]
+            hs, he = hnz[0], hnz[-1]
+            img = img[hs:he+1, ws:we+1]
+            if axid != 0:   # rotate 90
+                img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+            # set the alpha of non-brain region as 0
+            img[img[:,:,-1] == 1] = 0
+            cv2.imwrite(figname, img)
+
 
     def community_detection(self, load_partition=True):
         t0 = time.time()
@@ -60,12 +130,16 @@ class BrainParcellation:
         # Adjust n_neighbors based on your dataset and memory constraints
         coords = self.df[['soma_x', 'soma_y', 'soma_z']]
         feats = self.df[self.fnames].to_numpy()
+        #import ipdb; ipdb.set_trace()
+        
         # or try to use radius_neighbors_graph
-        #A = kneighbors_graph(feats, n_neighbors=20, include_self=True, mode='distance', metric='euclidean', n_jobs=8)
         # the radius are in 25um space
         radius_th = 10.
+        par1 = 2.
+        par2 = 5.
         partition_file = 'community_memberships.json'
 
+        #A = kneighbors_graph(coords, n_neighbors=50, include_self=True, mode='distance', metric='euclidean', n_jobs=8)
         A = radius_neighbors_graph(coords, radius=radius_th, include_self=True, mode='distance', metric='euclidean', n_jobs=8)
         print(f'[Neighbors generation]: {time.time() - t0:.2f} seconds')
         
@@ -74,14 +148,14 @@ class BrainParcellation:
         # estimate the edge weights
         dists = A_csr[sources, targets]
         dt = radius_th
-        wd = np.squeeze(np.asarray(np.exp(-2.*dists/dt)))
-        print('wd statis: ', wd.mean(), wd.max(), wd.min())
-        print(f'Total and avg number of edges: {wd.shape[0]}, {wd.shape[0]/feats.shape[0]}')
+        wd = np.squeeze(np.asarray(np.exp(-par1*dists/dt)))
+        print(f'wd[mean/max/min]: {wd.mean():.2f}, {wd.max():.2f}, {wd.min():.2f}')
+        print(f'Total and avg number of edges: {wd.shape[0]}, {wd.shape[0]/feats.shape[0]:.2f}')
 
         fs = feats[sources]
         ft = feats[targets]
-        wf = np.exp(-5. * np.linalg.norm(fs - ft, axis=1))
-        print('wf statis: ', wf.mean(), wf.max(), wf.min())
+        wf = np.exp(-par2 * np.linalg.norm(fs - ft, axis=1))
+        print(f'wf[mean/max/min]: {wf.mean():.2f}, {wf.max():.2f}, {wf.min():.2g}')
         print(f'[weights estimation]: {time.time() - t0:.2f} seconds')
 
         weights = wd * wf
@@ -95,9 +169,12 @@ class BrainParcellation:
             with open(partition_file, 'r') as fp:
                 community_memberships = json.load(fp)
             # Create a new partition object with the loaded memberships
-            partition = lg.RBConfigurationVertexPartition(g, membership=community_memberships)
+            partition = lg.RBConfigurationVertexPartition(g, 
+                                initial_membership=community_memberships, 
+                                weights=weights)
         else:
             ### Step 3: Apply the Leiden Algorithm
+            #partition = lg.find_partition(g, lg.ModularityVertexPartition, weights='weight')
             partition = lg.find_partition(g, lg.ModularityVertexPartition, weights='weight')
             print(f'[Partition]: {time.time() - t0: .2f} seconds')
 
@@ -111,7 +188,8 @@ class BrainParcellation:
 
         community_sizes = np.array([len(community) for community in partition])
         print(f'[Number of communities] = {len(partition)}')
-        print(f'[Community statistics]: {community_sizes.mean():.1f}, {community_sizes.std():.1f}, {community_sizes.max()}, {community_sizes.min()}')
+        print(f'[Number of large communities (n>5)] = {(community_sizes > 5).sum()}')
+        print(f'[Community statistics: mean/std/max/min]: {community_sizes.mean():.1f}, {community_sizes.std():.1f}, {community_sizes.max()}, {community_sizes.min()}')
         comms, counts = np.unique(community_sizes, return_counts=True)
         print(comms, counts)
 
@@ -121,14 +199,17 @@ class BrainParcellation:
 
         node_to_community = {node: community for node, community in enumerate(partition.membership)}
         # Initialize a dictionary to hold lists of nodes for each community
-        communities = defaultdict(list)
+        communities = defaultdict(list) # community to node
 
         # Populate the dictionary with node indices grouped by their community
         for node_index, community_index in enumerate(partition.membership):
             communities[community_index].append(node_index)
 
         # plot onto the CCF space
-        
+        dfp = coords.copy()
+        dfp['parc'] = partition.membership
+        self.visualize_on_ccf(dfp)
+        print()
         
 
 
@@ -136,9 +217,10 @@ if __name__ == '__main__':
     mefile = './data/mefeatures_100K.csv'
     scale = 25.
     full_features = False
+    load_partition = False
     
     bp = BrainParcellation(mefile, scale=scale, full_features=full_features)
-    bp.community_detection()
+    bp.community_detection(load_partition=load_partition)
     
 
 
